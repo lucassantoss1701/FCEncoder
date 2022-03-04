ccd4ecbce02549109186948d6a5e2e9e11bac398
Lucas Santos <lucassantos@luizalabs.com>
2022-03-04 17:47:09 -0300

    Video encoder

---
+++ application/services/video_service_test.go
@@ -0,0 +1,58 @@
+package services_test
+
+import (
+	"enconder/application/repositories"
+	"enconder/application/services"
+	"enconder/domain"
+	"enconder/framework/database"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/joho/godotenv"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func prepare() (*domain.Video, repositories.VideoRepository) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "thomas-o-trem-estourado.mp4"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+	repo.Insert(video)
+
+	return video, repo
+}
+
+func TestVideoServiceDownload(t *testing.T) {
+
+	video, repo := prepare()
+
+	videoService := services.NewVideoService()
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	err := videoService.Download("fc-enconder")
+	require.Nil(t, err)
+
+	err = videoService.Fragment()
+	require.Nil(t, err)
+
+	err = videoService.Encode()
+	require.Nil(t, err)
+
+	err = videoService.Finish()
+	require.Nil(t, err)
+}
